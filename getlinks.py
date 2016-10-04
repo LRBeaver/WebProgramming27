624d9ed6484092bacc5f7a98a65eb14b429c63ab
LRBeaver <GitPass16!>
2016-10-04 12:06:36 -0400

    Initial commit

---
+++ getlinks.py
@@ -0,0 +1,9 @@
+import htmllib, urllib, formatter
+website = urllib.urlopen("http://www.profmcmillan.com")
+data=website.read()
+website.close()
+format = formatter.AbstractFormatter(formatter.NullWriter())
+ptext = htmllib.HTMLParser(format)
+ptext.feed(data)
+for link in ptext.anchorlist:
+    print(link)
